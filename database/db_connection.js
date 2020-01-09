dcce63279694dd2e2424be69e8c9d1c10b2ead8e
Aravindan <aravindannichu007@gmail.com>
2020-01-09 22:33:36 +0530

    mangoose uri change

--- database/db_connection.js
+++ database/db_connection.js
@@ -4,7 +4,7 @@ const { mongoURI } = require('./config');
 
 // create DB connection
 const dbConnection = () => mongoose.connect(
-  mongoURI,
+  'mongodb://rasha:r123456@ds111455.mlab.com:11455/rashadb',
   {
     useNewUrlParser: true,
     useCreateIndex: true,
